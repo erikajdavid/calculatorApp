f3bbae0bfaffb0e59c6e4244bc1ccf000a1f05b2
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-05-13 15:20:00 -0400

    pseudo-code-ish

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//JS code
+//BASIC CALCULATOR APP
+
+//when the user clicks on any button, you want it to appear on the screen or perform an operation. 
+//target button and save in a variable. 
+const buttons = document.querySelectorAll('button');
+//add the eventListener to each button 
+buttons.addEventListener ('click', function() {
+});
+//a forEach(method) method somewehere, but where?
+
+//target screen and save in a variable
+//when a user clicks on a number, you want it to display on the screen
+const screenDisplay = document.querySelector('.screen');
+
+//you want to save the numbers the user clicks on in an array
+let calculation = [];
+
+//experiement with eval method. 
+
+//??????????????
